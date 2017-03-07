8136d39f014e5bfaaf07a42629da06caf1d81f68
rzcwade <renzicheng@yahoo.com>
2017-03-06 19:27:54 -0800

    Create ai_hw6_q9.py

---
+++ ai_hw6_q9.py
@@ -0,0 +1,13 @@
+from hw4standard import *
+
+# use freshvariables, unify, substitute
+
+kb = [
+        ['ostrich','sam'],
+        ['canary','tweety'],
+        [ ['bird','X'],['ostrich','X'] ],
+        [ ['bird','X'],['canary','X'] ],
+        [ ['fly','X'],['bird','X'],['normal','X'] ],
+        [ ['not',['normal','X']],['ostrich','X'] ],
+        [ ['normal','X'],['canary','X'] ],
+     ]
